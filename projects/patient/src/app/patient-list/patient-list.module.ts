85c5254db96bf81c458ed2a89540a6964063a787
SuyogGujarathi13 <suyog.gujarathi@triarqhealth.com>
2022-05-16 10:27:30 +0530

    First Commit

---
+++ projects/patient/src/app/patient-list/patient-list.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PatientListRoutingModule } from './patient-list-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PatientListRoutingModule
+  ]
+})
+export class PatientListModule { }
